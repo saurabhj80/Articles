ea8dc57fa6012b77b67e941061e8b0f3f599ccb6
saurabhj80 <saurabhj80@gmail.com>
2015-04-22 21:50:10 -0700

    Initial Commit

---
+++ Saurabh Jain/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Saurabh Jain
+//
+//  Created by Saurabh Jain on 4/22/15.
+//  Copyright (c) 2015 Saurabh Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Saurabh Jain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Saurabh Jain
+//
+//  Created by Saurabh Jain on 4/22/15.
+//  Copyright (c) 2015 Saurabh Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
